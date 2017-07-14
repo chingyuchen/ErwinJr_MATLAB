6ba72734fd46559e1a763078c6205a6d7476d18d
陳靜妤 <chingyuc@chenjingyude-MacBook-Air.local>
2017-07-13 21:14:17 -0400

    Stimulated Interaction functions are added

--- README.md
+++ README.md
@@ -4,15 +4,53 @@ The program calculates the confined energy levels and the spacial distribution o
 wave in the conduction band. The Schrodinger equation is solved based on the semiclassical approach. 
 That is, the entire heterostructure is treated as a single quantum mechanical system with a well-defined Hamiltonian. 
 The eigenstates are stationary without considering the coherent time evolution. The effective mass of the 
-electrons is energy and spacial dependent based on kp theory and the heterostructure of the material system. 
-The lifetime of the electron on a energy level is based on the LO phonon scattering lifetime. It is 
-calculated using Fermi's golden rule and Frolich Hamiltonian. 
+electrons is energy and spacial dependent based on the kp theory and the heterostructure of the material system. 
+The lifetime of the electron on a energy level is based on the logitudinal optical (LO) phonon and interfacr roughness 
+(IFR) scatterings. It is calculated using Fermi's golden rule and Frolich Hamiltonian. 
 
 
-## Energy Levels and Prbability Waves
+## Energy Levels and Probability Waves
+### Pre-calculation:
+None
 ### Input:
 Temperature, material system, layer structure, bias, number of period, strain 
 ### Output:
-Confined energy levels, the spacial distribution of the prbability wave
+All the confined energy levels, the spacial distribution of the prbability wave
 
 
+## Lifetimes, Dipole Moment and FoM
+### Pre-calculation:
+Energy levels and probability waves
+### Input:
+Two selected confined energy levels, temperature
+### Output:
+LO phonon scattering lifetimes, IFR scattering lifetimes, Optical dipole moment and FoM between the selected two energy levels
+
+
+## Steady State Electron Distribution
+### Pre-calculation:
+Energy levels and probability waves
+### Input:
+All the confined energy levels in the single target period, all the confined energy levels in the consecutive period, 
+doping density, doping area, upper and lower laser levels, temperature
+### Output:
+Initial electron distribution, steady state electron distribution on each of the confined energy level, 
+time evolution of the electron distribution, broadening, electroluminescence speactra
+
+
+## Scattering Current
+### Pre-calculation:
+Steady state electron distribution
+### Input:
+All the energy levels in two consecutive period, temperature
+### Output:
+Scattering current density
+
+
+## Stimulated Interaction
+### Pre-calculation:
+Energy levels and probability wave
+### Input:
+Selected interactive optical levels, spectra-overlap ratio, all the confined energy levels in the single target period, all the confined energy levels in the consecutive period, doping density, doping area, upper and lower laser levels, temperature
+### Output:
+Time evolution of the photon flux, time evolution of the electron distribution, steady state electron distribution
---
+++ dfluxCalInterTwo.m
@@ -0,0 +1,49 @@
+%{
+Copyright (c) 2012-2017, Ching-Yu Chen
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code or derivatives thereof must retain the 
+//       above copyright notice, this list of conditions and the following
+//       disclaimer.
+//     * Redistributions in binary form, and any binary file that is compiled 
+//       from a derivative of this source code, must reproduce the above 
+//       copyright notice, this list of conditions and the following disclaimer 
+//       in the documentation and/or other materials provided with the 
+//       distribution.
+//     * Any publication, presentation, or other publicly or privately 
+//       presented work having made use of or benefited from this software or 
+//       derivatives thereof must explicitly name Kale J. Franz in a section 
+//       dedicated to acknowledgements or name Kale J. Franz as a co-author 
+//       of the work.
+//     * Any use of this software that directly or indirectly contributes to 
+//       work or a product for which the user is or will be remunerated must be 
+//       further licensed through the Princeton Univeristy Office of Technology 
+//       Licensing and the Princeton Univeristy Mid-Infrared Photonics Lab led 
+//       by Professor Claire Gmachl prior to the transaction of said 
+//       remuneration.  
+// 
+// THIS SOFTWARE IS PROVIDED BY Ching-Yu Chen ''AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL Ching-Yu Chen BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+%}
+
+%{
+dflux/dt calculation when the flux only overlap with two other fluxes
+%}
+
+function [deltaFlux, stimHL, stimA, loss, spon] = dfluxCalInterTwo(fluxThis, fluxThat1, fluxThat2, rThis1, rThis2, rThat1, rThat2, dt, factorThis, factorThat1, factorThat2, sponFacThis, tauPhInvers, nH, nL, nA1, nA2)
+    stimHL = factorThis * (fluxThis * (nH - nL) + (fluxThat1 * rThat1 + fluxThat2 * rThat2) * nH); % stimluated emission contributed by itself and other transiton
+    stimA = factorThat1 * (fluxThis * rThis1 * nA1) + factorThat2 * (fluxThis * rThis2 * nA2); % absorbed by other optical transitions
+    loss = fluxThis * tauPhInvers; % mirror loss, waveguide loss
+    spon = sponFacThis * nH; % spontaneous emission
+    deltaFlux = dt * (stimHL - stimA - loss + spon); 
+end
